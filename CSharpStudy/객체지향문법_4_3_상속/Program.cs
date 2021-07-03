81df2edf6894acc290afdc9385f53f95c83e245a
Ws-Peroth <a01033056809@gmail.com>
2021-07-03 23:51:17 +0900

    ADD 상속 project

---
+++ CSharpStudy/객체지향문법_4_3_상속/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 객체지향문법_4_3_상속
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
