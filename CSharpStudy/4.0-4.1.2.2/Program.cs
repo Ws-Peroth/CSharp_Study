46d97ca3314ccfc1f0fa47b444921e00198b101a
Ws-Peroth <a01033056809@gmail.com>
2021-07-03 16:25:03 +0900

    클래스의 구성요소
    1 필드
    2 메소드
    3 생성자

---
+++ CSharpStudy/4.0-4.1.2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4._0_4._1._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharpStudy/클래스의 구성요소/ClassInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 클래스의_구성요소
+{
+    public class ClassInfo
+    {
+        private int data;   // 필드
+        private string name;
+
+        public ClassInfo(int data, string name) // 생성자
+        {
+            Console.WriteLine("생성자 : [객체 생성시 호출]");
+            this.data = data;   // 변수
+            this.name = name;
+        }
+
+        public void Method(int param)   // 메소드
+        {
+            Console.WriteLine("[메소드 호출]");
+            Console.WriteLine("Data : " + data + ", Name : " + name + ", Param : " + param);
+        }
+    }
+}
