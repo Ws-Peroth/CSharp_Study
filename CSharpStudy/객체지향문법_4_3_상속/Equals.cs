2c6829d5c43f7af45465aeb3d6e3b04879ed3643
Ws-Peroth <a01033056809@gmail.com>
2021-07-11 22:17:11 +0900

    Equals

---
+++ CSharpStudy/객체지향문법_4_3_상속/Equals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 객체지향문법_4_3_상속
+{
+    public class Equals
+    {
+        public static void RunMethod()
+        {
+
+        }
+    }
+}
