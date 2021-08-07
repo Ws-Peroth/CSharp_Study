08b39522a2d2791521cb4be6a891686b0e7ffb1e
Ws-Peroth <a01033056809@gmail.com>
2021-08-07 22:02:53 +0900

    Array_class

---
+++ CSharpStudy/객체지향문법_4_3_상속/Array_class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 객체지향문법_4_3_상속
+{
+    class Array_class
+    {
+        public static void RunMethod()
+        {
+            int[] array = new int[] { 1, 4, 2, 5, 3 };
+
+            Console.WriteLine($"array.Rank : {array.Rank}");
+            Console.WriteLine($"array.Rank : {array.Length}");
+            
+            Console.WriteLine($"array.Rank : {array.Rank}");
+        }
+    }
+}
